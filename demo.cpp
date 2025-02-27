453b5af0641cb45d70e478a9d903d7a6f5392bee
nitesh <niteshsingla00@gmail.com>
2025-02-27 09:38:46 +0530

    I/O

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,12 @@
-#include<iostream>
+# include<iostream>
 using namespace std;
+
 int main(){
-    cout<<"Hello World";
-    cout<<24;
+    int a,b;
+    cout<<"Enter a :\n";
+    cin>>a;
+    cout<<"Enter b :\n";
+    cin>>b;
+    cout<<"Sum is: "<<a+b;
     return 0;
 }
