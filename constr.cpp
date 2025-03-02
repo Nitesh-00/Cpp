be2853752585bb6e87cfccef36080d7bed43eb53
nitesh <niteshsingla00@gmail.com>
2025-03-02 21:16:53 +0530

    Finding distance

--- constr.cpp
+++ constr.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
-class Example{
-    int a;
-    int b;
+class Point{
+    int x;
+    int y;
     public:
-        Example(int x, int y);
-        void sum(){
-            cout<<"The sum is: "<<a+b<<endl;
-        }
+        Point(int a, int b);
+        friend void distance(Point o1, Point o2);
+        
 };
 
-Example :: Example(int x, int y){
-    a =x;
-    b =y;
+Point :: Point(int a, int b){
+    x = a;
+    y = b;
 }
+
+void distance(Point o1, Point o2){
+    float dis = sqrt(pow((o2.x - o1.x),2)+pow((o2.y - o1.y),2));
+    cout<<"Distance between q and q is "<<dis<<endl;  
+}
+
 int main(){
-    Example e(2,3);
-    e.sum();
+    Point p(4,2);
+    Point q(4,2);
+    distance(p,q);
     return 0;
 }
