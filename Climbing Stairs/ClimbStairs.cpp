057743c7869a419c900daed4161b8681a3b9c62b
nitesh <niteshsingla00@gmail.com>
2025-04-06 13:19:03 +0530

    Climbing Stairs Memoization

--- Climbing Stairs/ClimbStairs.cpp
+++ Climbing Stairs/ClimbStairs.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+#include<vector>
 
 int ClimbStairRec(int n){
     if(n==0 || n==1){
@@ -9,7 +10,23 @@ int ClimbStairRec(int n){
     return ClimbStairRec(n-1) + ClimbStairRec(n-2);
 }
 
+int ClimbStairMem(vector<int> &dp, int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+
+    if(dp[n] != -1){
+        return dp[n];
+    }
+
+    dp[n] = ClimbStairMem(dp,n-1) + ClimbStairMem(dp,n-2);
+    return dp[n];
+}
+
 int main(){
-    cout<<"Ways : "<<ClimbStairRec(4)<<endl;
+    int n=4;
+    vector<int> dp(n+1,-1);
+    cout<<ClimbStairMem(dp,n)<<endl;
+
     return 0;
 }
