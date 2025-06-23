a84f1da3cef52ea2a3ffecbf0e8298fce5d690ac
Nitesh-00 <niteshsingla00@gmail.com>
2025-06-23 15:40:06 +0530

    Pop Function Added

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 class Heap{
     vector<int> heap;
+    void heapify(int idx){
+        if(heap.size()==0) return;
+        int n=idx;
+        int ln=2*n+1;
+        int rn=2*n+2;
+        if(ln <heap.size() && heap[n]<heap[ln]){
+            n=ln;
+        }
+        if(rn <heap.size() && heap[n]<heap[rn]){
+            n=rn;
+        }
+        swap(heap[idx],heap[n]);
+        if(n!=idx){
+            heapify(n);
+        }
+    }
 public:
     void push(int val){
         heap.push_back(val);
@@ -16,6 +32,12 @@ public:
             child=parnt;
             parnt=(child-1)/2;
         }
+    } 
+
+    void pop(){
+        swap(heap[0],heap[heap.size()-1]);
+        heap.pop_back();
+        heapify(0);
     }
     
     int top(){
@@ -25,10 +47,23 @@ public:
 
 int main(){
     Heap h;
-    h.push(7);
+    h.push(9);
+    h.push(4);
     h.push(8);
-    h.push(3);
-    h.push(10);
-    cout<<h.top()<<" ";
+    h.push(1);
+    h.push(2);
+    h.push(5);
+    cout<<h.top()<<endl;
+    h.pop();
+    cout<<h.top()<<endl;
+    h.pop();
+    cout<<h.top()<<endl;
+    h.pop();
+    cout<<h.top()<<endl;
+    h.pop();
+    cout<<h.top()<<endl;
+    h.pop();
+    cout<<h.top()<<endl;
+
     return 0;
 }
